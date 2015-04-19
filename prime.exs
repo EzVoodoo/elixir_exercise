47750fd1711cf06f4eda2a60da2b2433f802898e
EzVoodoo <xz.mac@qq.com>
2015-04-19 16:30:35 +0800

    Prime number related
    Two functions which can list the prime number from 1 to n, and compute
    their sum using tail recursion.

---
+++ prime.exs
@@ -0,0 +1,25 @@
+defmodule Prime do
+	@doc """
+	This function computes the sum of the prime numbers from 1 to n
+	"""
+	def ps(n), do: _ps(n, 0)
+
+	defp _ps([], acc), do: acc
+	defp _ps([1|tail], acc), do: _ps(tail, acc+1)
+	defp _ps([head|tail], acc) do
+		_ps(Enum.reject(tail, &(rem(&1, head)==0)), head+acc)
+	end
+
+	@doc """
+	This function generates a list of prime numbers from 1 to n
+	"""
+	def prime(n), do: _prime(n, []) 
+	
+	defp _prime([], prime_list), do: Enum.reverse prime_list
+	defp _prime([1|tail], prime_list), do: _prime(tail, [1|prime_list])
+	defp _prime([head | tail], prime_list) do
+		_prime(Enum.reject(tail, &(rem(&1, head)==0)), [head|prime_list])
+	end
+	
+end
+
