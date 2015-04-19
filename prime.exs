b9c6d9d3e863e33a5530ebf5af38faefeecad230
EzVoodoo <xz.mac@qq.com>
2015-04-19 17:19:20 +0800

    Interface optimization

--- prime.exs
+++ prime.exs
@@ -2,7 +2,7 @@ defmodule Prime do
 	@doc """
 	This function computes the sum of the prime numbers from 1 to n
 	"""
-	def ps(n), do: _ps(n, 0)
+	def ps(n), do: _ps(Enum.to_list(1..n), 0)
 
 	defp _ps([], acc), do: acc
 	defp _ps([1|tail], acc), do: _ps(tail, acc+1)
@@ -13,12 +13,14 @@ defmodule Prime do
 	@doc """
 	This function generates a list of prime numbers from 1 to n
 	"""
-	def prime(n), do: _prime(n, []) 
+	def prime(n), do: _prime(Enum.to_list(1..n), []) 
 	
 	defp _prime([], prime_list), do: Enum.reverse prime_list
 	defp _prime([1|tail], prime_list), do: _prime(tail, [1|prime_list])
 	defp _prime([head | tail], prime_list) do
 		_prime(Enum.reject(tail, &(rem(&1, head)==0)), [head|prime_list])
+		##IO.inspect head
 	end
+	
 end
 
